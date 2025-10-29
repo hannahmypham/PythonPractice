c991ea7ae8bad933ce80e01ff198a9cac31525da
HannahPham <han.tmphm@gmail.com>
2025-10-28 22:33:17 -0700

    Added new files for Python practice

--- ProbHW2.py
+++ ProbHW2.py
@@ -1,17 +1,34 @@
 #ProbHW2
 
-"""Use numpy.random or any other of your favorite library to generate uniform random
-variables between [0, 1]. Suppose X¯
-n be the sample mean of n such random variables. Find mean µ and
-variance of X¯
-n. Let Zn =
-X¯ √ n−µ
-Var(X¯n)
-. For n = 100, do 100 trials of this experiment, and plot the number
-of times Zn ≤ a for a = −2, −1, −0.5, −0.25.0, 0.25, 0.5, 1, 2. Plot the cumulative distribution function
-of N (0, 1) and compare.
-"""
-
 import numpy as np
-numbers_array = np.random.uniform(50)
-print(numbers_array)
+import matplotlib.pyplot as plt
+from scipy.stats import norm 
+
+# Calculating population mean and variance 
+variance_pop = ((1-0)**2)/12
+mu = (0+1)/2
+
+# Choose random variables 
+np.random.seed(42)
+numbers_array = np.random.uniform(size=(100,100))
+a = np.array([-2, -1, -0.5, -0.25, 0, 0.25, 0.5, 1, 2])
+
+
+sample_mean=np.mean(numbers_array, axis = 1)
+sample_var = variance_pop/100
+Z_n = (sample_mean- mu) / np.sqrt(sample_var)
+
+P_emp = []
+P_theo = []
+
+print("Z_n",Z_n)
+
+for i in a:
+    P_emp.append(np.sum(Z_n <= i) / 100)
+    P_theo.append(norm.cdf(i))
+
+print("Sample mean is ",sample_mean)
+print("Sample variance is ", sample_var)
+print(P_emp)
+print(P_theo)
+
